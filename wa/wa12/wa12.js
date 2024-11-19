d943205e7782bf11f12315038e85a1b44338e879
forrestjordan <166655676+forrestjordan@users.noreply.github.com>
2024-11-18 23:56:13 -0700

    wa12

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -2,32 +2,38 @@ console.log(ourObject);
 
 let myData = {};
 
-function fetchData(){
+function fetchData() {
     const comicNumber = Math.floor(Math.random() * 3000) + 1;
-fetch('https://corsproxy.io/?https://xkcd.com/${comicNumber}/info.0.json')
-    .then(res =>{
-        if(res.ok){
-            return res.json();
-        }else{
-            console.log(res);
-        }
-    }).then(res => {
-        myData = res;
-        console.log(myData);
-        document.getElementById("comicTitle").innerHTML = myData.title;
-        document.getElementById("comicImg").src = myData.img;
-        document.getElementById("comicAltText").setAttribute("alt",myData.alt);
+    fetch(`https://corsproxy.io/?https://xkcd.com/${comicNumber}/info.0.json`)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                console.log(res);
+            }
+        })
+        .then(res => {
+            myData = res;
+            console.log(myData);
+            document.getElementById("comicTitle").innerHTML = myData.title;
+            document.getElementById("comicImg").src = myData.img;
+            document.getElementById("comicAltText").innerText = myData.alt; 
+            document.getElementById("comicImg").alt = myData.alt;
 
-        let m = myData.month;
-        let d = myData.day;
-        let y = myData.year;
-        document.getElementById("comicDate").innerHTML = (m + '/' + d + '/' + y);
-
-    })
-    .catch(error => {console.log(error)})
+            let m = myData.month;
+            let d = myData.day;
+            let y = myData.year;
+            document.getElementById("comicDate").innerHTML = `${m}/${d}/${y}`;
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
+
 fetchData();
 
-document.getElementById("generate").addEventListener("click", e=> {fetchData();})
+document.getElementById("generate").addEventListener("click", () => {
+    fetchData();
+});
 
 console.log(myData);
